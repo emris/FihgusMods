9d8e0ffc16c6d47967f16b61fac6a0f7515c0d1d
Doug DeVicariis <koo_04@yahoo.com>
2013-05-13 21:46:05 -0700

    Base Teleportation code

--- fihgu/core/functions/Teleport.java
+++ fihgu/core/functions/Teleport.java
@@ -1,5 +1,29 @@
 package fihgu.core.functions;
 
-public class Teleport {
+import fihgu.core.elements.CommandBase;
 
+public class Teleport LoginCommand extends CommandBase{
+	
+	public static ComandBase instance;
+	
+	public Teleport(){
+		name = "Teleport";
+		instance = this;
+	}
+	
+	public void teleportToPlayer(Player from, Player to){
+		
+	}
+	
+	public void teleportToLoc(Player who, int x, int y, int z){
+		
+	}
+	
+	public void teleportToLoc(Player who, int x, int z){
+		
+	}
+	
+	public void teleportToSpawn(Player who){
+		
+	}
 }
