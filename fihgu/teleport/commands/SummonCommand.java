24ef5540fb0d00b9bbd98655e17c9cfd12331ba6
Doug DeVicariis <koo_04@yahoo.com>
2013-05-16 08:30:12 -0700

    More to Summon

--- fihgu/teleport/commands/SummonCommand.java
+++ fihgu/teleport/commands/SummonCommand.java
@@ -2,11 +2,16 @@ package fihgu.teleport.commands;
 
 import net.minecraft.entity.player.EntityPlayerMP;
 import fihgu.core.elements.CommandBase;
+import fihgu.core.elements.Player;
+import fihgu.core.elements.Request;
 import fihgu.core.functions.Language;
+import fihgu.core.functions.Warp;
 
 public class SummonCommand extends CommandBase{
+	Warp warp;
 	public SummonCommand()
 	{
+		warp = new Warp();
 		name = "summon";
 		usage = Language.translate(" <Player name>: Summon a player to yourself.");
 	}
@@ -14,14 +19,22 @@ public class SummonCommand extends CommandBase{
 	@Override
 	public void processPlayer(EntityPlayerMP player, String[] args)
 	{
+		Player player1 = new Player(player);
+		Player player2;
 		if(args.length < 1 || args.length > 1)
 		{
-			player.sendChatToPlayer(Language.translate("Invalad command arguments."));
-			player.sendChatToPlayer(Language.translate("Usage: /summon <Player Name>"));
+			player1.msg(Language.translate("Invalad command arguments."));
+			player1.msg(Language.translate("Usage: /summon <Player Name>"));
 		}
 		else
 		{
-			
+			player2 = new Player(args[0]);
+			new Request(player1,30){
+				public void onAccept()
+				{
+					
+				}
+			};
 		}
 	}
 }
