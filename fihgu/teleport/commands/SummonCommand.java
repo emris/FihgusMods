3769c45fb5fe13f8f329ed60a61c691b90a44860
Doug DeVicariis <koo_04@yahoo.com>
2013-05-16 08:16:25 -0700

    Summon in progress
    Partial Summon

--- fihgu/teleport/commands/SummonCommand.java
+++ fihgu/teleport/commands/SummonCommand.java
@@ -7,14 +7,20 @@ import fihgu.core.functions.Language;
 public class SummonCommand extends CommandBase{
 	public SummonCommand()
 	{
-		name = "warp";
+		name = "summon";
 		usage = Language.translate(" <Player name>: Summon a player to yourself.");
 	}
 	
 	@Override
 	public void processPlayer(EntityPlayerMP player, String[] args)
 	{
-		if(args.length <= 1){
+		if(args.length < 1 || args.length > 1)
+		{
+			player.sendChatToPlayer(Language.translate("Invalad command arguments."));
+			player.sendChatToPlayer(Language.translate("Usage: /summon <Player Name>"));
+		}
+		else
+		{
 			
 		}
 	}
